acc922a6be9634dce17664c3715087df599fa01b
Dennis Bhayu Bramastha <dennisbayu99@gmail.com>
2024-09-02 12:01:32 +0700

    perubahan function index  pada people controller

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -16,16 +16,17 @@ class PeopleController extends Controller
     }
 
 	public function index(){
-	
+
 		$queryset = People::all();
-		return view('people.index', compact('queryset'));
+        $person = People::all();
+		return view('people.index', compact('queryset', 'person'));
 	}
-	
+
 	public function create(){
 		$organizations = Organization::all();
 		return view('people.create', compact('organizations'));
 	}
-	
+
 	public function store(Request $request){
 		// dd($request);
 		$request->validate([
@@ -38,12 +39,12 @@ class PeopleController extends Controller
 		People::create($request->all());
 
 		return redirect()->route('people.index')->with('success', 'People created successfully');
-		
+
 	}
 
 	public function edit($personId){
 		$person = People::findOrFail($personId);
-		
+
 		$organizations = Organization::all();
 		return view('people.edit', compact('person', 'organizations'));
 	}
@@ -66,5 +67,5 @@ class PeopleController extends Controller
 		$people->delete();
 		return redirect()->route('people.index')->with('success', 'People deleted successfully');
 	}
-	
+
 }
